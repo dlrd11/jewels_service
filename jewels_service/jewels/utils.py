eb06022bcf4bbaabeee403acb1c21972a2bfa97f
dlrd11 <50637478+dlrd11@users.noreply.github.com>
2024-07-21 19:20:54 -0500

    Development (#7)
    * :wrench: adding django files to be configuration (#1) (#2)
    
    Co-authored-by: dlrd11 <dromerod@DESKTOP-MJCGL96>
    
    * Config rest (#3)
    
    * :wrench: adding django files to be configuration
    
    * :heavy_plus_sign: updating
    
    ---------
    
    Co-authored-by: dlrd11 <dromerod@DESKTOP-MJCGL96>
    
    * :sparkles: adding the functionality for a crud (#4)
    
    Co-authored-by: dlrd11 <dromerod@DESKTOP-MJCGL96>
    
    * Crud jewels (#5)
    
    * :sparkles: adding the functionality for a crud
    
    * :heavy_plus_sign: adding depency for unit tests
    
    * :heavy_plus_sign: adding depency for unit tests
    
    * :necktie: adding new functionalities for permissions, and authetication
    
    * quit at breakpoint
    
    * :memo: adding swagger functionality
    
    * :memo: updating the readme
    
    ---------
    
    Co-authored-by: dlrd11 <dromerod@DESKTOP-MJCGL96>
    
    * :alien: adding docker
    
    * adding a url with deploy
    
    ---------
    
    Co-authored-by: dlrd11 <dromerod@DESKTOP-MJCGL96>

--- jewels_service/jewels/utils.py
+++ jewels_service/jewels/utils.py
@@ -4,7 +4,8 @@ import requests
 from rest_framework import exceptions
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-AUTH_SERVICE_URL = "http://0.0.0.0:8000/auth/verify"
+# AUTH_SERVICE_URL = "http://0.0.0.0:8000/auth/verify"
+AUTH_SERVICE_URL = "https://web-production-e9b0.up.railway.app/auth/verify"
 
 
 def verify_token(token):
