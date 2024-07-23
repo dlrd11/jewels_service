0ad73c272291069920857cc7dd982be88c36053e
dlrd11 <dromerod@DESKTOP-MJCGL96.>
2024-07-22 20:26:59 -0500

    update host

--- jewels_service/jewels/utils.py
+++ jewels_service/jewels/utils.py
@@ -5,7 +5,7 @@ from rest_framework import exceptions
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 # AUTH_SERVICE_URL = "http://0.0.0.0:8000/auth/verify"
-AUTH_SERVICE_URL = "https://web-production-e9b0.up.railway.app/auth/verify"
+AUTH_SERVICE_URL = "http://0.0.0.0:8000/auth/verify"
 
 
 def verify_token(token):
